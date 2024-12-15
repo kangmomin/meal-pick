e27a6232946638bc7fb26adc2cd1c2230e6ba1fd
koldin108902 <alex108902@naver.com>
2024-12-16 03:17:26 +0900

    Feat: java version and kotlin version upgrade

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.3.4"
+	kotlin("jvm") version "2.1.0"
+	id("org.jetbrains.kotlin.plugin.spring") version "2.1.0"
+	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.6"
 	id("org.asciidoctor.jvm.convert") version "3.3.2"
-	kotlin("plugin.jpa") version "1.9.25"
+	kotlin("plugin.jpa") version "2.1.0"
 }
 
 group = "com.mealpick"
@@ -12,7 +12,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
+		languageVersion = JavaLanguageVersion.of(18)
 	}
 }
 
@@ -37,9 +37,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	// open ai
 	implementation("org.springframework.ai:spring-ai-openai-spring-boot-starter")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("com.fasterxml.jackson.core:jackson-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("com.squareup.okhttp3:okhttp")
 	implementation ("io.jsonwebtoken:jjwt-api:0.11.2")
 	implementation ("io.jsonwebtoken:jjwt-jackson:0.11.2")
 
